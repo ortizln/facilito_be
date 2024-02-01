656d6d9a1fe6f4340a4da28014682e0cb4f06c2d
ortizln <ortizln9@gmail.com>
2024-02-01 14:50:49 -0500

    create backend

---
+++ src/main/java/com/api/facilitocobros/FacilitoCobrosApplication.java
@@ -0,0 +1,13 @@
+package com.api.facilitocobros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilitoCobrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilitoCobrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/facilitocobros/FacilitoCobrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.facilitocobros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacilitoCobrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
